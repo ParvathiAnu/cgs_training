76d6065457592db6dbd8025f00d12eb0f9a518bb
Nagulapalli Anu vinaya lakshmi parvathi <113771607+ParvathiAnu@users.noreply.github.com>
2024-08-23 23:28:16 +0530

    prime number.py

--- prime number.py
+++ prime number.py
@@ -1,10 +1,15 @@
-prime=int(input("enter a number"))
-flag=True
-for i in range(2,prime):
-    if prime%i==0:
-        flag=False
-        break
-if flag  ==True:
-    print("the number is prime")
+prime = int(input("Enter a number: "))
+
+if prime < 1:
+    print("The number is not a prime.")
 else:
-    print("the number is not a prime")
+    flag = True
+    for i in range(2, prime):
+        if prime % i == 0:
+            flag = False
+            break
+    
+    if flag == True:
+        print("The number is prime")
+    else:
+        print("The number is not a prime")
