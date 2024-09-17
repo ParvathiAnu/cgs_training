17af7c7f67d1237d167115f75f7941f7c27517d6
Nagulapalli Anu vinaya lakshmi parvathi <113771607+ParvathiAnu@users.noreply.github.com>
2024-09-17 20:57:29 +0530

    Add files via upload

--- api/api.js
+++ api/api.js
@@ -1,27 +1,31 @@
-window.onload = async function() {
-    try {
-        var result = await fetch("https://randomuser.me/api/");
-        var data = await result.json();
-        var userData = data.results[0];
-        console.log(userData);
+window.onload = function() {
+    var btn = document.getElementById("btn");
 
-        var image = document.getElementById("image");
-        image.src = userData.picture.large;
+    async function funname() {
+        try {
+            var result = await fetch("https://randomuser.me/api/");
+            var data = await result.json();
+            var userData = data.results[0];
+            console.log(userData);
 
-        var name = document.getElementById("name");
-        name.innerText = `Name: ${userData.name.first} ${userData.name.title}`;
+            var image = document.getElementById("image");
+            image.src = userData.picture.large;
+  
 
-        var btn = document.getElementById("btn");
-        btn.addEventListener("click", function() {
-              window.location.reload();
-        });
+            var name = document.getElementById("name");
+            name.innerText = `Name:${userData.name.title}  ${userData.name.first} `;
 
-        var address = document.getElementById("address");
-        address.innerText = `Street Number: ${userData.location.street.number}\nStreet Name: ${userData.location.street.name}\nCity: ${userData.location.city}\nState: ${userData.location.state}\nCountry: ${userData.location.country}`;
-    
-        var details = document.getElementById("details");
-        details.innerText = ` DOB: ${userData.dob.date}\nAge: ${userData.dob.age}\nEmail: ${userData.email}\nGender: ${userData.gender}`;
-    } catch (error) {
-        console.error("Error fetching user data:", error);
+            var address = document.getElementById("address");
+            address.innerText = `Street Number: ${userData.location.street.number}\nStreet Name: ${userData.location.street.name}\nCity: ${userData.location.city}\nState: ${userData.location.state}\nCountry: ${userData.location.country}`;
+
+            var details = document.getElementById("details");
+            details.innerText = `DOB: ${userData.dob.date}\nAge: ${userData.dob.age}\nEmail: ${userData.email}\nGender: ${userData.gender}`;
+
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+        }
     }
+
+    funname();
+    btn.addEventListener("click", funname);
 };
